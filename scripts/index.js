ed30710586705842f766f4fd7778f8a4cdd82038
Andrey Arkhipov <arhipov@yandex.ru>
2021-12-20 18:22:09 +0300

    Add icon-road, new custom priperty css,refactoring js

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,13 @@ const arrowRightSlider = document.querySelector(".slider__button-right");
 const textsSlider = document.querySelectorAll(".slider__text-container");
 const imagesSlider = Array.from(document.querySelectorAll(".slider__image"));
 const imageContainerSlider = document.querySelector(".slider__image-container");
+const timeChangeImage =
+  parseFloat(getComputedStyle(imageContainerSlider).getPropertyValue("--time-change-image")) * 1000;
+const iconsRoadSlider = [
+  "../images/highway-line.svg",
+  "../images/gravel-line.svg",
+  "../images/tt-line.svg",
+];
 
 //function
 function setCustomProperties(prop, val) {
@@ -30,7 +37,7 @@ burgerBtn.addEventListener("click", () => {
   }
 });
 
-//chenge theme
+//change theme
 switcherThems.addEventListener("click", () => {
   const sunImage = document.querySelector(".switcher-thems__image_type_sun");
   const monthImage = document.querySelector(".switcher-thems__image_type_month");
@@ -54,6 +61,7 @@ switcherThems.addEventListener("click", () => {
 });
 
 //slider
+
 let positionVisibleSliderText = 0;
 let positionVisibleSliderImage = 3;
 let imageWidth = parseFloat(getComputedStyle(imagesSlider[0]).width);
@@ -61,7 +69,7 @@ let positionImagesSlider = [];
 for (let i = -6; i <= 6; i++) {
   positionImagesSlider.push(imageWidth * i + 40 * i);
 }
-resetPositionImageContainer(1000);
+resetPositionImageContainer(timeChangeImage);
 
 function changeTextSlider(mathStr) {
   textsSlider[positionVisibleSliderText].classList.remove("slider__text-container_visible");
@@ -73,26 +81,24 @@ function changeTextSlider(mathStr) {
   }
 
   textsSlider[positionVisibleSliderText].classList.add("slider__text-container_visible");
+  changeIconRoadSlider(positionVisibleSliderText);
+}
+
+function changeIconRoadSlider(position) {
+  setCustomProperties("--road-icon", `url(${iconsRoadSlider[position]})`);
 }
 
 function changeImageSlider(direction) {
   imageContainerSlider.style = "";
   let transformValue;
-  let x;
-  if (direction === "right") {
-    positionVisibleSliderImage--;
-    x = positionImagesSlider[positionVisibleSliderImage];
-    transformValue = `translate(${x}px,0)`;
-    imageContainerSlider.style.transform = transformValue;
-  } else {
-    positionVisibleSliderImage++;
-    x = positionImagesSlider[positionVisibleSliderImage];
-    transformValue = `translate(${x}px,0)`;
-    imageContainerSlider.style.transform = transformValue;
-  }
+  let numberPx;
 
+  direction === "right" ? positionVisibleSliderImage-- : positionVisibleSliderImage++;
+  numberPx = positionImagesSlider[positionVisibleSliderImage];
+  transformValue = `translate(${numberPx}px,0)`;
+  imageContainerSlider.style.transform = transformValue;
   if (positionVisibleSliderImage === 6 || positionVisibleSliderImage === 0) {
-    resetPositionImageContainer(1000);
+    resetPositionImageContainer(timeChangeImage);
   }
 }
 
@@ -121,6 +127,6 @@ window.addEventListener("resize", () => {
     positionImagesSlider.push(imageWidth * i + 40 * i);
   }
   imageContainerSlider.style.transition = "none";
-  let x = positionImagesSlider[positionVisibleSliderImage];
-  imageContainerSlider.style.transform = `translate(${x}px,0)`;
+  let numberPx = positionImagesSlider[positionVisibleSliderImage];
+  imageContainerSlider.style.transform = `translate(${numberPx}px,0)`;
 });
